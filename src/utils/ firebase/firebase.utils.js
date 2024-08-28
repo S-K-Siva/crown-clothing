17930fddffcc03325770a81ef23193087da94b44
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2024-08-28 11:49:35 +0530

    Update firebase.utils.js

--- src/utils/ firebase/firebase.utils.js
+++ src/utils/ firebase/firebase.utils.js
@@ -1,108 +1,103 @@
 import {initializeApp} from 'firebase/app';
-import {getAuth,GoogleAuthProvider,signInWithPopup,createUserWithEmailAndPassword,
-signInWithEmailAndPassword,signOut,onAuthStateChanged} from 'firebase/auth'
-
-import {getFirestore,doc,getDoc,setDoc,collection, writeBatch,query,getDocs} from 'firebase/firestore';
+// import { getAnalytics } from "firebase/analytics";
+import {getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, createUserWithEmailAndPassword} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {doc,getDoc,setDoc, getFirestore,collection,writeBatch,query,getDocs} from "firebase/firestore";
+// onAuthStateChangedListenr
+import { onAuthStateChanged } from 'firebase/auth';
+import { signOut } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyD7wj7ILoPv9HRj2QizFIBugePccWvND4o",
-  authDomain: "crown-clothing-dfb5f.firebaseapp.com",
-  projectId: "crown-clothing-dfb5f",
-  storageBucket: "crown-clothing-dfb5f.appspot.com",
-  messagingSenderId: "681791253570",
-  appId: "1:681791253570:web:6eb6a8261cf3bba0f751db"
+  apiKey: "AIzaSyCOWRNy_dOw5NyVECk6R_tO-r-rIjOsC3s",
+  authDomain: "crown-clothing2-app.firebaseapp.com",
+  projectId: "crown-clothing2-app",
+  storageBucket: "crown-clothing2-app.appspot.com",
+  messagingSenderId: "371736631032",
+  appId: "1:371736631032:web:3165a8896f8cb71133cd0d",
+  measurementId: "G-ZB1NXR1NNP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-console.log(app);
 
 const provider = new GoogleAuthProvider();
+
 provider.setCustomParameters({
-    prompt:"select_account",
+    prompt : "select_account"
 });
 
 export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth,provider);
 export const db = getFirestore();
 
-export const dbWorkLoad = async (collectionName, data) => {
+export const createUserDocumentsFromAuth = async(userAuth) => {
+    const userDocRef = doc(db,'users',userAuth.uid);
+
+    const userSnapShot = await getDoc(userDocRef);
+
+
+    // if user data exists, just return the user reference
+    if(!userSnapShot.exists()){
+        try{
+            const {displayName, email} = userAuth;
+            const createdAt = new Date();
+            await setDoc(userDocRef, {
+                displayName : displayName,
+                email : email,
+                createdAt : createdAt
+            })
+        }catch(error){
+            console.log("Error while creating user Snapshot");
+        }
+    }
+    // if user exists already
+    return userDocRef;
+}
+
+// create user with an email and password
+export const createAuthUserWithEmailAndPassword = async(email,password) => {
+    if(!email || !password) return;
+    return await createUserWithEmailAndPassword(auth,email,password);
+}
+
+// auth state listner
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth,callback);
+
+export const signOutUser =  () => signOut(auth);
+
+
+export const addCollectionsAndDocuments = async (collectionName, objectsToAdd) => {
     const collectionRef = collection(db,collectionName);
     const batch = writeBatch(db);
 
-    data.forEach((obj) => {
-        const docRef = doc(collectionRef,obj.title.toLowerCase());
-        batch.set(docRef,obj);
+
+    objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef,object.title.toLowerCase());
+        batch.set(docRef,object);
     });
-    
 
     await batch.commit();
-    console.log("done");
-}
-export const createUserWithGooglePopUp = async (user) => {
-    const getUserRef = doc(db,'users',user.uid);
-
-    const getSnapshot = await getDoc(getUserRef);
-
-    if(!getSnapshot.exists()){
-        const {displayName, email} = user;
-        const createdAt = new Date();
-        const createUserRef = await setDoc(getUserRef,
-            {
-                displayName,
-                email,
-                createdAt
-            });
-        return createUserRef;
-    }else{
-        return getUserRef;
-    }
+    console.log("Data is pushed into firestore!");
 }
 
-export const getDataFromFireStore = async (collectionName) => {
-    const collectionRef = collection(db,collectionName);
-    const Q = query(collectionRef);
-    const snapShot = await getDocs(Q);
-    const finalData = snapShot.docs.reduce((acc,cur) => {
-        const {title ,items} = cur.data();
-        // console.log(cur);
-        acc[title]= items;
+export const getCategoryAndDocuments = async() => {
+    const collectionRef = collection(db,'categories');
+
+    // first intialize the query for the collection
+    const q = query(collectionRef);
+    // we getting the SnapShot(location of all the data/queries of the 'categories' collection)
+    const querySnapshot = await getDocs(q);
+
+    const categoryMap = querySnapshot.docs.reduce((acc,item)=>{
+        const {title,items} = item.data();
+        acc[title] = items;
         return acc;
     },{});
-    return finalData;
-}
-export const signInWithGooglePopUp = () => {
-    return signInWithPopup(auth,provider);
-};
-
-export const createAuthWithEmailAndPassword = async (email,password,displayName) => {
-    if(!email || !password) return;
-    try{
-    const response = await createUserWithEmailAndPassword(auth,email,password);
-    const {user} = response;
-    console.log(user);
     
-    const userRef = doc(db,'users',user.uid);
-    
-    const userSnapshot = await getDoc(userRef);
-    if(!userSnapshot.exists()){
-        const createdAt = new Date();
-        const newUserRef = await setDoc(userRef,{
-            displayName,
-            email,
-            createdAt
-        });
-        newUserRef['displayName'] = displayName;
-        return newUserRef;
-    }
-    else{
-        return userRef;
-    }
-}
-    catch(err){
-        alert("Already the account is created with this email id.");
-        return 'error';
-    }
-    
+    return categoryMap;
 }
 
 export const signAuthWithEmailAndPassword = async (email,password) =>{
